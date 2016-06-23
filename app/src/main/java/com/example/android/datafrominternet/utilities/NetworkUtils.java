c93385df2fea2db7f71fe5ee8565e0e7accbad41
mlustig <mlustig@google.com>
2016-06-23 15:18:40 -0700

    T02.04-Exercise-ConnectingToTheInternet

--- app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
@@ -48,7 +48,6 @@ public class NetworkUtils {
      * @return The URL to use to query the weather server.
      */
     public static URL buildUrl(String githubSearchQuery) {
-        // COMPLETED (1) Fill in this method to build the proper Github query URL
         Uri builtUri = Uri.parse(GITHUB_BASE_URL).buildUpon()
                 .appendQueryParameter(PARAM_QUERY, githubSearchQuery)
                 .appendQueryParameter(PARAM_SORT, sortBy)
