080edbe487988c5bbba61445b9f12fc54e3494d9
Lyla <lyla@udacity.com>
2016-10-20 22:15:53 -0700

    T06.11-FinalState

--- app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -27,7 +27,6 @@ import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
 import android.widget.Toast;
 
-// COMPLETED (1) Implement OnPreferenceChangeListener
 public class SettingsFragment extends PreferenceFragmentCompat implements
         OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener {
 
@@ -52,8 +51,6 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
             }
         }
 
-        // COMPLETED (3) Add the OnPreferenceChangeListener specifically to the EditTextPreference
-        // Add the preference listener which checks that the size is correct to the size preference
         Preference preference = findPreference(getString(R.string.pref_size_key));
         preference.setOnPreferenceChangeListener(this);
     }
@@ -91,11 +88,6 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
             preference.setSummary(value);
         }
     }
-
-    // COMPLETED (2) Override onPreferenceChange. This method should try to convert the new preference value
-    // to a float; if it cannot, show a helpful error message and return false. If it can be converted
-    // to a float check that that float is between 0 (exclusive) and 3 (inclusive). If it isn't, show
-    // an error message and return false. If it is a valid number, return true.
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
